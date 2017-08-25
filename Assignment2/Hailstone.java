6ad6c7e6dfe1509a211de96cc06630a97ef803db
tomtom28 <tomthompson828@gmail.com>
2017-08-24 21:57:50 -0400

    finished assign 2

--- Assignment2/Hailstone.java
+++ Assignment2/Hailstone.java
@@ -9,8 +9,46 @@
 import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
+	
 	public void run() {
-		/* You fill this in */
+		
+		// Step Counter
+		int counter = 0;
+		
+		// Collect user input
+		print("Gimme a (positive) number: ");
+		int n = readInt();
+		
+		while (n > 1) {
+			// Even
+			if (n%2 == 0) {
+				n = isEven(n);
+				counter++;
+			}
+			// Odd
+			else {
+				n = isOdd(n);
+				counter++;
+			}
+		}
+		
+		// After loop, show final message
+		println("The process took " + counter + " steps to complete.");
+		
 	}
+	
+	// If n is even, divide it by two. 
+	private int isEven(int n) {
+		println(n + " is even, so I take half and give back ... " + n/2);
+		return n/2;
+	}
+	
+	// If n is odd, multiply it by three and add one
+	private int isOdd(int n) {
+		println( n + " is odd, so I make 3n+1 and give back ... " + (3*n+1) );
+		return n*3 + 1;
+	}
+	
+	
 }
 
